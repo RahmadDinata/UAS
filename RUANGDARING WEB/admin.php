334c9c7b4f2a5b4cb09a6daf4ad55c97b5ba3ddc
RahmadDinata <115675724+RahmadDinata@users.noreply.github.com>
2023-01-08 19:07:34 +0800

    "UAS PEMROGRAMAN BERKERANGKA KERJA"

---
+++ RUANGDARING WEB/admin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="admin.css">
+<head>
+    <title>Admin Page</title>
+</head>
+<body>
+<div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+
+<nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Ruang<span>Belajar</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#teams" class="menu-btn">Tutor</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+                
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+</nav>
+
+
+
+<section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello Admin, Welcome to</div>
+                <div class="text-2">Ruang Belajar</div>
+                <div class="text-3"><span class="typing"></span></div>
+
+
+                <a href="tabel.php">tabel</a>
+                <a href="index.php">Logout</a>
+                
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    
+    
+</body>
+</html>
